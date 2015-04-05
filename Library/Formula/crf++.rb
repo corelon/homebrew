24df9c7f825648b0b0ce58e9275bbd02d0fc7257
Nikos Tzanos <corelon@gmail.com>
2015-04-05 22:37:32 +0100

    Update crf++.rb
    changed sha1 check

--- Library/Formula/crf++.rb
+++ Library/Formula/crf++.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Crfxx < Formula
   homepage 'http://code.google.com/p/crfpp/'
   url 'https://drive.google.com/uc?id=0B4y35FiV1wh7QVR6VXJ5dWExSTQ&export=download'
-  sha1 '979a686a6d73d14cdd0c96a310888fb6bffd2e91'
+  sha1 '9f4782563ac82bfde39ac433d0e5a2871b23516a'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
